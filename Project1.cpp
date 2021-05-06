22e8996eeb9d933a188af70490dd540ac99e161f
Cameryn Duarte <82423065+camerynd@users.noreply.github.com>
2021-05-06 12:16:36 -0600

    Add files via upload

---
+++ Project1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Name: Cameryn Duarte" << endl;
+    cout << "Email: duarte.camery17@gmail.com" << endl;
+
+    return 0;
+}
